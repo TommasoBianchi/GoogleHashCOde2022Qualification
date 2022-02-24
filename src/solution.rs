6947de96574265d999ca40c3e676d42b4abc82fd
Tommaso Bianchi <tommaso4.bianchi@mail.polimi.it>
2022-02-24 19:47:25 +0100

    Implement solution data types and formatting

--- src/solution.rs
+++ src/solution.rs
@@ -1,16 +1,37 @@
 use std::fmt::{Debug, Display};
 
-pub struct Solution {
-    // TODO: implement
+use crate::parse_input::{Contributor, Project};
+
+pub struct Solution<'a> {
+    pub executed_projects: Vec<ExecutedProject<'a>>,
 }
 
-impl Display for Solution {
+pub struct ExecutedProject<'a> {
+    pub project: &'a Project,
+    pub contributors: Vec<&'a Contributor>,
+}
+
+impl<'a> Display for Solution<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        f.write_fmt(format_args!("{}\n", self.executed_projects.len()))?;
+        for executed_project in self.executed_projects.iter() {
+            f.write_fmt(format_args!("{}\n", executed_project.project.name))?;
+            f.write_fmt(format_args!(
+                "{}\n",
+                executed_project
+                    .contributors
+                    .iter()
+                    .map(|c| c.name.as_str())
+                    .collect::<Vec<_>>()
+                    .join(" ")
+            ))?;
+        }
+
+        Ok(())
     }
 }
 
-impl Debug for Solution {
+impl<'a> Debug for Solution<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         todo!()
     }
