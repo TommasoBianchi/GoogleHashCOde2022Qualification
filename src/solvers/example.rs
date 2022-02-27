da48ef6775859e29c5b498bb4e5dafa3019a4cc0
Tommaso Bianchi <tommaso.bianchi@gmail.com>
2022-02-27 17:25:16 +0100

    Remove example solver

--- src/solvers/example.rs
+++
@@ -1,7 +0,0 @@
-use crate::{parse_input::InputData, solution::Solution};
-
-use super::errors::SolveError;
-
-pub fn solve(input: &InputData) -> Result<Solution, SolveError> {
-    todo!()
-}
--- src/solvers/mod.rs
+++ src/solvers/mod.rs
@@ -1,3 +1,2 @@
 mod errors;
-pub mod example;
 pub mod greedy;
